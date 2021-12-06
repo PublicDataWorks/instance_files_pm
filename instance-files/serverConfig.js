8c2c35477b09f6ecd05ee2efb407b74ea5feb04d
Karan Krishnani <krkrishn@thoughtworks.com>
2021-12-06 12:03:44 -0500

    Karan [#180034570] adjusts CSP for API Gateway

--- instance-files/serverConfig.js
+++ instance-files/serverConfig.js
@@ -26,7 +26,7 @@ module.exports = {
         "https://b.tile.openstreetmap.org",
         "https://api.mapbox.com",
         "https://www.google-analytics.com",
-        "https://hggkf95dtf.execute-api.us-east-1.amazonaws.com"
+        "https://3nt5k6bwpd.execute-api.us-east-1.amazonaws.com/"
       ]
     },
     corsOrigin: "https://police-data-manager-demo.herokuapp.com",
